0b7cdc13763430aaf6a32a6067381ceb11200dc7
Ian Morland <ian@morland.me>
2022-06-22 23:23:20 +0100

    prettier

--- js/src/admin/components/ExtensionSettingsPage.js
+++ js/src/admin/components/ExtensionSettingsPage.js
@@ -2,43 +2,43 @@ import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
 
 export default class ExtensionSettingsPage extends ExtensionPage {
-    content() {
-        return [
-            <div className="container">
-                <div className="OpenCollectiveSettings">
-                    <p>
-                        {app.translator.trans('fof-open-collective.admin.settings.desc', {
-                            a: <a href="https://opencollective.com/applications" target="_blank" />,
-                        })}
-                    </p>
+  content() {
+    return [
+      <div className="container">
+        <div className="OpenCollectiveSettings">
+          <p>
+            {app.translator.trans('fof-open-collective.admin.settings.desc', {
+              a: <a href="https://opencollective.com/applications" target="_blank" />,
+            })}
+          </p>
 
-                    {this.buildSettingComponent({
-                        type: 'text',
-                        setting: 'fof-open-collective.api_key',
-                        label: app.translator.trans('fof-open-collective.admin.settings.api_key_label'),
-                    })}
+          {this.buildSettingComponent({
+            type: 'text',
+            setting: 'fof-open-collective.api_key',
+            label: app.translator.trans('fof-open-collective.admin.settings.api_key_label'),
+          })}
 
-                    {this.buildSettingComponent({
-                        type: 'text',
-                        setting: 'fof-open-collective.slug',
-                        label: app.translator.trans('fof-open-collective.admin.settings.slug_label'),
-                    })}
+          {this.buildSettingComponent({
+            type: 'text',
+            setting: 'fof-open-collective.slug',
+            label: app.translator.trans('fof-open-collective.admin.settings.slug_label'),
+          })}
 
-                    <div className="Form-group">
-                        {this.buildSettingComponent({
-                            type: 'select',
-                            setting: 'fof-open-collective.group_id',
-                            options: app.store.all('groups').reduce((o, g) => {
-                                o[g.id()] = g.nameSingular();
+          <div className="Form-group">
+            {this.buildSettingComponent({
+              type: 'select',
+              setting: 'fof-open-collective.group_id',
+              options: app.store.all('groups').reduce((o, g) => {
+                o[g.id()] = g.nameSingular();
 
-                                return o;
-                            }, {}),
-                            label: app.translator.trans('fof-open-collective.admin.settings.group_label'),
-                        })}
-                    </div>
-                    {this.submitButton()}
-                </div>
-            </div>,
-        ];
-    }
+                return o;
+              }, {}),
+              label: app.translator.trans('fof-open-collective.admin.settings.group_label'),
+            })}
+          </div>
+          {this.submitButton()}
+        </div>
+      </div>,
+    ];
+  }
 }
