83b8d7cbda7239a791f4e2d8f0335f37fdd23114
Ian Morland <ian@morland.me>
2022-06-22 23:22:33 +0100

    Webpack5

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 import ExtensionSettingsPage from './components/ExtensionSettingsPage';
 
-app.initializers.add('fof/open-collective', (app) => {
+app.initializers.add('fof/open-collective', () => {
     app.extensionData.for('fof-open-collective').registerPage(ExtensionSettingsPage);
 });
